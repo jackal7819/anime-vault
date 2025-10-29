69ec6b532b71cdaed584f66c1026ead31e0d87f3
jackal7819 <jackal3166@gmail.com>
2025-10-29 16:45:41 +0200

    style: change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,12 @@ import './globals.css';
 import Footer from '@/components/Footer';
 import Hero from '@/components/Hero';
 import type { Metadata } from 'next';
-import { Montserrat_Alternates, Russo_One } from 'next/font/google';
+import { Montserrat_Alternates } from 'next/font/google';
 
 const montserratAlternates = Montserrat_Alternates({
 	subsets: ['latin'],
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 	variable: '--font-montserrat',
-});
-
-const russoOne = Russo_One({
-	subsets: ['latin'],
-	weight: ['400'],
-	variable: '--font-russo',
 });
 
 export const metadata: Metadata = {
@@ -24,7 +18,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang='en' className={`${montserratAlternates.variable} ${russoOne.variable}`}>
+		<html lang='en' className={montserratAlternates.className}>
 			<body>
 				<main className='max-w-7xl mx-auto bg-[#0F1117]'>
 					<Hero />
