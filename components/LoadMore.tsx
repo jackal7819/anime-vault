d9ab5ac7a938cbf8a831c78d109d6d8b7ec207a7
jackal7819 <jackal3166@gmail.com>
2025-10-31 19:00:56 +0200

    feat: add pagination

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,37 @@
+'use client';
+
 import Image from 'next/image';
+import { fetchAnime } from '@/app/action';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+import AnimeCard, { AnimeProp } from './AnimeCard';
+
+let page = 2;
 
 export default function LoadMore() {
+	const { ref, inView } = useInView();
+	const [data, setData] = useState<AnimeProp[]>([]);
+
+	useEffect(() => {
+		if (inView) {
+			fetchAnime(page)
+				.then((res) => {
+					setData([...data, ...res]);
+					page++;
+				})
+				.catch((error) => console.log(error));
+		}
+	}, [inView, data]);
 	return (
 		<>
+			<section className='grid grid-cols-1 gap-10 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2'>
+				{data.map((item: AnimeProp, index: number) => (
+					<AnimeCard key={item.id} anime={item} index={index} />
+				))}
+			</section>
 			<section className='flex items-center justify-center w-full'>
-				<div>
+				<div ref={ref}>
 					<Image
 						src='./spinner.svg'
 						alt='spinner'
